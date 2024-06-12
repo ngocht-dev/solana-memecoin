b5322102b630cb4b1d5b2a84ce3b1fa57ec64013
ngocht2 <ngocht@blockchainlabs.asia>
2024-06-12 10:02:14 +0700

    update metadata

--- memecoin-spl/scripts/metadata.ts
+++ memecoin-spl/scripts/metadata.ts
@@ -1,7 +1,7 @@
 export default {
   name: "Crazy Doge",
   symbol: "CZDOGE",
-  uri: "",
+  uri: "https://raw.githubusercontent.com/ngocht-dev/solana-memecoin/main/memecoin-spl/metadata.json",
   decimals: 8,
   supply: 100000000,
 };
